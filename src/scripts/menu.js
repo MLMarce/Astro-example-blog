96866d4b53f38031bfb874a86849e4f842cd00ad
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-01-26 18:09:44 -0300

    pasamos los scripts a un archivo a parte

---
+++ src/scripts/menu.js
@@ -0,0 +1,5 @@
+let hamburgerElement = document.querySelector(".hamburger");
+      hamburgerElement.addEventListener("click", (e) => {
+        document.querySelector(".nav").classList.toggle("expanded");
+        hamburgerElement.classList.toggle('closer');
+      });
